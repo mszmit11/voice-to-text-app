22928f9421a97c8458140c209c17b77e05e05079
vmarcin <mszmit11@gmail.com>
2023-04-24 13:23:06 +0200

    remove comment in run.py and rename file

---
+++ run/run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import wave
+import sys
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+SetLogLevel(0)
+
+wf = wave.open(sys.argv[1], "rb")
+if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print("Audio file must be WAV format mono PCM.")
+    sys.exit(1)
+
+model = Model(r"M:\pythonProject\vosk-project\model")
+
+rec = KaldiRecognizer(model, wf.getframerate())
+rec.SetWords(True)
+rec.SetPartialWords(True)
+
+while True:
+    data = wf.readframes(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
